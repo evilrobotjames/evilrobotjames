fdb1ab5ec45a15d9e5cb04aee4055b1e9f41c4e5
James <evilrobotjames@gmail.com>
2015-10-27 19:32:35 +0000

    Quick play with an erasmus sieve.

---
+++ seive/seive.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+MAX = 10240
+
+primes = [ True for _ in range(0, MAX) ]
+
+def print_primes(primes):
+    render = []
+    for i in range(0, MAX):
+        if primes[i]:
+            render.append(i)
+    print render
+
+for i in range(2, MAX): # ignore zero and one
+    print "doing {}".format(i)
+    if primes[i]:
+        j = i + i
+        while j < MAX:
+#            if primes[i]:
+#                print "eliminating {}".format(j)
+#            else:
+#                print "{} already eliminated".format(j)
+            primes[j] = False
+            j = j + i
+        print_primes(primes)
+    else:
+        pass
+        #print "{} already not a prime".format(i)
