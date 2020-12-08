86e71e08f492c469c2e3289333290d8ef535ac8d
James Hansen <jhansen@symphony.ncipher.com>
2020-12-08 09:29:37 +0000

    AoC 7-2

--- advent-of-code-2020/7/7-2.py
+++ advent-of-code-2020/7/7-2.py
@@ -4,7 +4,7 @@
 import re
 
 rule_re = re.compile('^(?P<colour>[a-z]+ [a-z]+) bags contain (?P<contents>[0-9a-z, ]+)[.]$')
-inner_bag_re = re.compile('^(?P<count>[0-9]+) (?P<colour>[a-z]+ [a-z]+) bag[s]?$')
+inner_bag_re = re.compile('^(?P<number>[0-9]+) (?P<colour>[a-z]+ [a-z]+) bag[s]?$')
 
 with open('data.txt') as file:
     rules = file.read().splitlines()
@@ -21,7 +21,7 @@ for rule in rules:
     colour = rule_match['colour']
     assert(not colour in rules_dict)
 
-    inner_bags = set()
+    inner_bags = []
 
     contents = rule_match['contents']
     if contents != 'no other bags':    
@@ -29,33 +29,20 @@ for rule in rules:
         for contents in contents_list:
             inner_bag_match = inner_bag_re.match(contents)
             assert(inner_bag_match)
-            inner_bags.add(inner_bag_match['colour'])
+            inner_bags.append((inner_bag_match['colour'], int(inner_bag_match['number'])))
 
     rules_dict[colour] = inner_bags
     
-# Search for "shiny gold"
+# Count bags within 'shiny gold' bag"
 
-class FoundException(Exception):
-    pass
 
-colours_that_can_contain = []
+def sum_bag(colour):
+    sum = 1
+    inner_bags = rules_dict[colour]
+    for inner_colour, inner_number in rules_dict[colour]:
+        sum += inner_number * sum_bag(inner_colour)
+    return sum
 
-def traverse(bag):
-    if bag == 'shiny gold':
-        raise FoundException()
-    else:
-        for inner_bag in rules_dict[bag]:
-            traverse(inner_bag)
-
-for start_bag in rules_dict:
-    if start_bag == 'shiny gold':
-        continue
-    try:
-        traverse(start_bag)
-    except FoundException:
-        colours_that_can_contain.append(start_bag)
-
-print(colours_that_can_contain)
-print(len(colours_that_can_contain))
+print(sum_bag('shiny gold'))
 
 
